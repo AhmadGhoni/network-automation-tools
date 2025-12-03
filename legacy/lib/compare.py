29df8e1e44274b7efbbe6dc29e097f07eebdc061
fzl96 <fpsdragon333@gmail.com>
2025-12-03 18:58:45 +0700

    fix: Change comparison result dir

--- legacy/lib/compare.py
+++ legacy/lib/compare.py
@@ -377,10 +377,10 @@ def compare(base_dir=None):
     customer_name = get_customer_name()
     devices = load_devices()
     if base_dir:
-        path = os.path.join(base_dir, "legacy", "compare")
+        path = os.path.join(base_dir, customer_name, "legacy", "compare")
         snapshot_path = os.path.join(base_dir, customer_name, "legacy", "snapshot")
     else:
-        path = os.path.join("results", "legacy", "compare")
+        path = os.path.join("results", customer_name, "legacy", "compare")
         snapshot_path = os.path.join("results", customer_name, "legacy", "snapshot")
 
     os.makedirs(path, exist_ok=True)
